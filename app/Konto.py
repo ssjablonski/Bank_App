4335035eb75c68041d0787d8ee8f681d0cc371b1
Sebastian Jabłoński <sjablonski2@inf.ug.edu.pl>
2023-10-11 17:21:35 +0200

    fetures 1-4 laby

--- app/Konto.py
+++ app/Konto.py
@@ -1,3 +1,22 @@
 class Konto:
-    def __init__(self):
-        pass
+    def __init__(self, imie, nazwisko, pesel, promo_code = None):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.saldo = 0
+        if len(pesel) != 11:
+            self.pesel = "Niepoprawny pesel!"
+        else:
+            self.pesel = pesel
+
+        if self.is_promo_code_correct(promo_code):
+            self.saldo = 50
+        else:
+            self.saldo = 0
+
+    def is_promo_code_correct(self, promo_code):
+        if promo_code is None:
+            return False
+        if promo_code.startswith("PROM_") and len(promo_code) == 8:
+            return True
+        else:
+            return False
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -3,11 +3,46 @@ import unittest
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
+    imie = "Dariusz"
+    nazwisko = "Januszewski"
+    pesel = "12345678901"
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto(self.imie, self.nazwisko, self.pesel)
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, self.pesel, "Pesel nie zostal napisany")
+    
+    def test_pesel_with_len_10(self):
+        konto = Konto(self.imie, self.nazwisko, "1234567890")
+        self.assertEqual(konto.pesel, "Niepoprawny pesel!", "Za krótki pesel został przujęty za prawidłowy")
 
+    def test_pesel_with_len_12(self):
+        konto = Konto(self.imie, self.nazwisko, "123456789000")
+        self.assertEqual(konto.pesel, "Niepoprawny pesel!", "Za długi pesel został przujęty za prawidłowy")
+
+    def test_empty_pesel(self):
+        konto = Konto(self.imie, self.nazwisko, "")
+        self.assertEqual(konto.pesel, "Niepoprawny pesel!", "Za krótki pesel został przyjęty za prawidłowy")
+
+    def test_promo_wrong_prefix(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, "prom_123")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest zerowe!")
+
+    def test_promo_wrong_suffix(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, "PROM_1233")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest zerowe!")
+
+    def test_promo_wrong_len(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, "PROM_")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest zerowe!")
+
+    def test_promo_correct(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, "PROM_123")
+        self.assertEqual(konto.saldo, 50, "Promocja nie została naliczona!")
+
+    
+
+    
     #tutaj proszę dodawać nowe testy
