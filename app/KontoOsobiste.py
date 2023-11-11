590b4f99db3fc9ec307cc6fecc097b25d376992c
Sebastian Jablonski <s.jablonski.684@studms.ug.edu.pl>
2023-11-11 18:42:50 +0100

    feature 12

--- app/KontoOsobiste.py
+++ app/KontoOsobiste.py
@@ -43,20 +43,28 @@ class KontoOsobiste(Konto):
         else:
             return False
 
-    def zaciagnij_kredyt(self, kwota):
+    def warunek_3_ostatnie(self):
         last3 = self.historia[-3:]
-        last5 = self.historia[-5:]
         if len(self.historia) < 3:
             self.zgoda_na_kredyt = False
             return False
         elif all(element > 0 for element in last3):
-            self.saldo+=kwota
             self.zgoda_na_kredyt=True
             return True
-        elif len(self.historia) < 5:
+    
+    def warunek_5_ostatnie(self, kwota):
+        last5 = self.historia[-5:]
+        if len(self.historia) < 5:
             self.zgoda_na_kredyt = False
             return False
         elif sum(last5) > kwota:
             self.saldo+= kwota
             self.zgoda_na_kredyt = True
             return True
+
+    def zaciagnij_kredyt(self, kwota):
+        if self.warunek_3_ostatnie() or self.warunek_5_ostatnie(kwota):
+            return True
+        else:
+            return False
+            
--- app/tests/test_loans.py
+++ app/tests/test_loans.py
@@ -1,7 +1,7 @@
 import unittest
+from parameterized import parameterized
 from ..Konto import Konto
 from ..KontoOsobiste import KontoOsobiste
-from ..KontoFirmowe import KontoFirmowe
 
 class TestLoans(unittest.TestCase):
     personal_data = {
@@ -9,42 +9,18 @@ class TestLoans(unittest.TestCase):
         "surname" : "Januszewski",
         "pesel" : "02070803628"
     }
-
-    def test_last_3_transactions(self):
+    @parameterized.expand([
+        ([100,100,100], 250, True),
+        ([-100,100,100], 250, False),
+        ([100,100], 250, False),
+        ([100, -100, 500, -1, -10], 300, True),
+        ([100, -100, 100, -1, -10], 300, False),
+        ([100, -100, 500, -1], 300, False)
+    ])
+    def test_last_3_transactions(self, historia, kwota_kredytu, oczekwiany_wynik):
         konto=KontoOsobiste(self.personal_data["name"], self.personal_data["surname"], self.personal_data["pesel"])
-        konto.historia = [100, 100, 100]
-        konto.zaciagnij_kredyt(250)
-        self.assertEqual(konto.zgoda_na_kredyt, True, "Zgoda powinna zostać udzielona")
-
-    def test_last_3_transactions_not_incoming(self):
-        konto=KontoOsobiste(self.personal_data["name"], self.personal_data["surname"], self.personal_data["pesel"])
-        konto.historia = [-100, 100, 100]
-        konto.zaciagnij_kredyt(250)
-        self.assertEqual(konto.zgoda_na_kredyt, False, "Ostatnie 3 tranzakcje nie sa wplatami")    
-
-    def test_last_3_transactions_not_3(self):
-        konto=KontoOsobiste(self.personal_data["name"], self.personal_data["surname"], self.personal_data["pesel"])
-        konto.historia = [100, 100]
-        konto.zaciagnij_kredyt(250)
-        self.assertEqual(konto.zgoda_na_kredyt, False, "Ostatnie 3 tranzakcje nie sa wplatami")    
-
-
-    def test_sum_5_transactions(self):
-        konto=KontoOsobiste(self.personal_data["name"], self.personal_data["surname"], self.personal_data["pesel"])
-        konto.historia = [100, -100, 500, -1, -10]
-        konto.zaciagnij_kredyt(300)
-        self.assertEqual(konto.zgoda_na_kredyt, True, "Zgoda powinna zostać udzielona")
-
-    def test_sum_5_transactions_failed(self):
-        konto=KontoOsobiste(self.personal_data["name"], self.personal_data["surname"], self.personal_data["pesel"])
-        konto.historia = [-100, -100, 100, -1, -10]
-        konto.zaciagnij_kredyt(300)
-        self.assertEqual(konto.zgoda_na_kredyt, False, "Zgoda nie powinna zostać udzielona")
-
-    def test_sum_5_transactions_to_small_history(self):
-        konto=KontoOsobiste(self.personal_data["name"], self.personal_data["surname"], self.personal_data["pesel"])
-        konto.historia = [100, -100, 500, -1]
-        konto.zaciagnij_kredyt(300)
-        self.assertEqual(konto.zgoda_na_kredyt, False, "Historia konta jest za krótka!")
+        konto.historia = historia
+        konto.zaciagnij_kredyt(kwota_kredytu)
+        self.assertEqual(konto.zgoda_na_kredyt, oczekwiany_wynik, "Zgoda powinna zostać udzielona")
 
     
